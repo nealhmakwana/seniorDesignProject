497507ad5abbefeb54604b390f9fc2f153ba9d51
nealhmakwana <123991235+nealhmakwana@users.noreply.github.com>
2024-02-28 13:17:13 -0600

    redesigned profile page to match the rest of the app (#22)
    * redesigned profile page to match the rest of the app
    
    * reformated and resolved warnings.
    
    ---------
    
    Co-authored-by: talatcanoguz <talatcanoguz0@gmail.com>

--- lib/views/screens/profile_view.dart
+++ lib/views/screens/profile_view.dart
@@ -10,39 +10,108 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
-
   @override
   Widget build(BuildContext context) {
     final userViewModel = Provider.of<UserViewModel>(context);
+    final userInfoMap = userToMap(userViewModel);
 
     return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0, left: 20.0, bottom: 20.0),
-            child: Text(
-              'Profile',
-              style: TextStyle(
-                fontSize: 35.0,
-                fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment:
+                CrossAxisAlignment.start, // Ensures left alignment
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 20.0, left: 20.0),
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    // This shapes the card with rounded corners
+                    borderRadius: BorderRadius.circular(
+                        10), // Match the borderRadius as in your provided code
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment
+                          .start, // Ensures left alignment within the card
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment
+                              .start, // Ensures the row content is left-aligned
+                          children: [
+                            Icon(Icons.person, color: Colors.black),
+                            SizedBox(width: 8),
+                            Text(
+                              'General Information',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        ...userInfoMap.entries.map((entry) {
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment
+                                .start, // Ensures left alignment for each entry
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 8.0),
+                                child: RichText(
+                                  text: TextSpan(
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black, // Default text color
+                                    ),
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                          text: '${entry.key}: ',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      TextSpan(
+                                        text: entry.value.toString(),
+                                        style: const TextStyle(
+                                            color: Colors
+                                                .blue), // Value text color
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              if (entry != userInfoMap.entries.last)
+                                const Divider(),
+                            ],
+                          );
+                        }).toList(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          ...userToMap(userViewModel).entries.map((entry) {
-            return Card(
-              child: ListTile(
-                title: Text(entry.key),
-                subtitle: Text(entry.value.toString()),
-              ),
-            );
-          }).toList(),
-        ],
+        ),
       ),
     );
   }
 
-  static Map<String, dynamic> userToMap(UserViewModel userViewModel){
-    if(userViewModel.user.isPatient!) {
+  static Map<String, dynamic> userToMap(UserViewModel userViewModel) {
+    if (userViewModel.user.isPatient!) {
       return {
         'First Name': userViewModel.user.firstName,
         'Last Name': userViewModel.user.lastName,
@@ -54,24 +123,22 @@ class _ProfileViewState extends State<ProfileView> {
         'Injury Type': userViewModel.user.injuryType,
         'Date of Injury': userViewModel.user.dateOfInjury,
         'Past Injuries': userViewModel.user.pastInjuries,
-        'Total Workouts': userViewModel.user.totalWorkouts
+        'Total Workouts': userViewModel.user.totalWorkouts,
       };
-    }
-    else if(!userViewModel.user.isPatient!){
+    } else if (!userViewModel.user.isPatient!) {
       return {
         'First Name': userViewModel.user.firstName,
         'Last Name': userViewModel.user.lastName,
         'Email Address': userViewModel.user.email,
         'Hospital Name': userViewModel.user.hospitalName,
-        'Hospital Address' : userViewModel.user.hospitalAddress,
-        'Hospital City' : userViewModel.user.hospitalCity,
-        'Hospital State' : userViewModel.user.hospitalState,
-        'Hospital Zip Code' : userViewModel.user.hospitalZipCode,
-        'Hospital Floor Room' : userViewModel.user.hospitalFloorRoom,
-        'Specialization' : userViewModel.user.specialization,
+        'Hospital Address': userViewModel.user.hospitalAddress,
+        'Hospital City': userViewModel.user.hospitalCity,
+        'Hospital State': userViewModel.user.hospitalState,
+        'Hospital Zip Code': userViewModel.user.hospitalZipCode,
+        'Hospital Floor Room': userViewModel.user.hospitalFloorRoom,
+        'Specialization': userViewModel.user.specialization,
       };
-    }
-    else{
+    } else {
       return <String, dynamic>{};
     }
   }
